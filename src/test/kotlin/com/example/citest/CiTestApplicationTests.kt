597c7064cfefe9a7d386a5a469ae6bbe8f460d07
sk.kwon <sk.kwon@battleent.com>
2022-02-24 16:55:31 +0900

    Initial commit

---
+++ src/test/kotlin/com/example/citest/CiTestApplicationTests.kt
@@ -0,0 +1,19 @@
+package com.example.citest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+    @Test
+    fun trueTest() {
+        assert(true)
+    }
+
+    @Test
+    fun falseTest() {
+        assert(false)
+    }
+
+}
