7135fac2983b3a2770dd6ebc2324e6e226959a77
sk.kwon <sk.kwon@battleent.com>
2022-03-15 15:33:59 +0900

    root gradle 수정 함

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
